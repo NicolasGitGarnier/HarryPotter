eb70db185a49d1b499c36341fc0a179fcde45ab4
nicolabrico <niga61368@eleve.isep.fr>
2023-02-24 14:41:14 +0100

    Initial commit

---
+++ src/main/java/org/example/composants/Potion.java
@@ -0,0 +1,4 @@
+package org.example.composants;
+
+public class Potion {
+}
---
+++ src/main/java/org/example/composants/Wizard.java
@@ -0,0 +1,11 @@
+package org.example.composants;
+
+import java.util.List;
+
+public class Wizard {
+    private class pet(Pet);
+    private class wand(Wand);
+    private class house(House);
+    private class knownSpells(List<Spell>);
+    private class potion(List<Potion>);
+}

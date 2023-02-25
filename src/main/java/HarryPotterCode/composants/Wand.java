4223ef2968bddb2d4cc16f965ed7d642d2ea3594
nicolabrico <niga61368@eleve.isep.fr>
2023-02-25 09:53:38 +0100

    Création des classes dans Wand

--- src/main/java/HarryPotterCode/composants/Wand.java
+++ src/main/java/HarryPotterCode/composants/Wand.java
@@ -1,4 +1,17 @@
 package HarryPotterCode.composants;
 
 public class Wand {
+    private static class core {
+        private int size;
+        public core(int size) {
+            this.size = size;
+        }
+    }
+    public record Size(int length) {
+        public Size {
+            if (length < 0) {
+                throw new IllegalArgumentException("La longueur doit être positive");
+            }
+        }
+    }
 }
--- src/main/java/HarryPotterCode/composants/Wizard.java
+++ src/main/java/HarryPotterCode/composants/Wizard.java
@@ -3,9 +3,9 @@ package HarryPotterCode.composants;
 import java.util.List;
 
 public class Wizard {
-    private class pet(Pet);
-    private class wand(Wand);
-    private class house(House);
-    private class knownSpells(List<Spell>);
-    private class potion(List<Potion>);
+    private class pet(Pet){}
+    private class wand(Wand){}
+    private class house(House){}
+    private class knownSpells(List<Spell>){}
+    private class potion(List<Potion>){}
 }

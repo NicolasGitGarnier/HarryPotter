5f65fb59a16cda773b5c92ec155c3ebb7ceb2f53
nicolabrico <niga61368@eleve.isep.fr>
2023-02-25 15:07:21 +0100

    Intro Poudlard Effect

--- src/main/java/HarryPotterCode/Main.java
+++ src/main/java/HarryPotterCode/Main.java
@@ -19,9 +19,22 @@ public class Main {
         Wand wand = new Wand(Wand.getCore(), Wand.getWandSize());
         Wizard wizard = new Wizard( firstName, lastName, null, wand, SortingHat.assignHouse(), new ArrayList<>(),new ArrayList<>() );
 
-        System.out.println("Bonjour "+ wizard.getFirstName() + " " + wizard.getLastName() + ", bienvenue à Poudlard !");
-        System.out.println("La baguette magique vous correspondant le mieux est en "+ wand.getCore() + " et de taille "+wand.getWandSize());
-        System.out.println("Le Choipeau Magic vous a attribué à la maison " + wizard.getHouse());
+        //Start of the aventure
+        System.out.println("");
+        System.out.println("");
+        System.out.println("Bonjour "+ wizard.getFirstName() + " " + wizard.getLastName() + ", et bienvenue à Poudlard !");
+        System.out.println("Votre baguette magique est en "+ wand.getCore() + " de taille "+wand.getWandSize());
+        System.out.println("Vous appartenez à la maison " + wizard.getHouse());
+        System.out.println("Que l'aventure commence !");
+        System.out.println("________________________________________________________________________________________________");
+        System.out.println("  _____                _ _               _               _                 _                  ");
+        System.out.println(" |  __ \\              | | |             | |     /\\      | |               | |                 ");
+        System.out.println(" | |__) |__  _   _  __| | | __ _ _ __ __| |    /  \\   __| |_   _____ _ __ | |_ _   _ _ __ ___ ");
+        System.out.println(" |  ___/ _ \\| | | |/ _` | |/ _` | '__/ _` |   / /\\ \\ / _` \\ \\ / / _ \\ '_ \\| __| | | | '__/ _ \\");
+        System.out.println(" | |  | (_) | |_| | (_| | | (_| | | | (_| |  / ____ \\ (_| |\\ V /  __/ | | | |_| |_| | | |  __/");
+        System.out.println(" |_|   \\___/ \\__,_|\\__,_|_|\\__,_|_|  \\__,_| /_/    \\_\\__,_| \\_/ \\___|_| |_|\\__|\\__,_|_|  \\___|");
+        System.out.println("________________________________________________________________________________________________");
+
 
     }
 }
--- src/main/java/HarryPotterCode/composants/Wand.java
+++ src/main/java/HarryPotterCode/composants/Wand.java
@@ -4,7 +4,6 @@ import java.util.Random;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import lombok.experimental.UtilityClass;
 
 @Data
 @NoArgsConstructor

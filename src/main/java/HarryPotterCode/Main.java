1e132362768515e9975b46e7ffaebbb69469b489
nicolabrico <niga61368@eleve.isep.fr>
2023-02-25 18:53:24 +0100

    Following of the intro -> going to the first level

--- src/main/java/HarryPotterCode/Main.java
+++ src/main/java/HarryPotterCode/Main.java
@@ -8,16 +8,15 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome into Harry Potter's World...");
+        System.out.println("Welcome into Harry Potter's World... First of all, who are you ?");
         // Creating the main character
         System.out.print("Enter your first name : ");
         String firstName = sc.nextLine();
         System.out.print("Enter your last name : ");
         String lastName = sc.nextLine();
-
+        // Setting the data of the character
         Wand wand = new Wand(Wand.getCore(), Wand.getWandSize());
         Wizard wizard = new Wizard( firstName, lastName, null, wand, SortingHat.assignHouse(), new ArrayList<>(),new ArrayList<>() );
-
         //Start of the aventure
         System.out.println("");
         System.out.println("");
@@ -29,15 +28,21 @@ public class Main {
         System.out.println(" | |  | (_) | |_| | (_| | | (_| | | | (_| |  / ____ \\ (_| |\\ V /  __/ | | | |_| |_| | | |  __/");
         System.out.println(" |_|   \\___/ \\__,_|\\__,_|_|\\__,_|_|  \\__,_| /_/    \\_\\__,_| \\_/ \\___|_| |_|\\__|\\__,_|_|  \\___|");
         System.out.println("________________________________________________________________________________________________");
-        System.out.println("Hi "+ wizard.getFirstName() + " " + wizard.getLastName()+ ".");
-        System.out.println("Welcome to Hogwart !");
-        System.out.println("Your wand is made of "+ wand.getCore() + " and is "+wand.getWandSize()+ " cm long.");
-        System.out.println("You are in the " + wizard.getHouse()+ " house.");
+        System.out.println("Hello "+ wizard.getFirstName() + " " + wizard.getLastName()+ ".\n"+
+                "Welcome to Poudlard !\n"+
+                "Your wand is made of "+ wand.getCore() + " and is "+wand.getWandSize()+ " cm long.\n"+
+                "You are in the " + wizard.getHouse()+ " house.");
         //Choose your Pet
         Pet pet = Pet.getPet();
         wizard.setPet(pet);
-        System.out.println("Now, you own a " + pet.name().toLowerCase() + ".");
-        System.out.println("Let's start the adventure !");
+        System.out.println("Great now you own a " + pet.name().toLowerCase() + ".");
 
+        System.out.println("");
+        System.out.println("Let's start the adventure !");
+        System.out.println("Bilan of the data :"+ wizard.getFirstName() + " " + wizard.getLastName() + " " + wizard.getPet() + " " + wizard.getHouse() + " " + wizard.getPotions() + " " + wizard.getKnownSpells());
+        System.out.println("");
+
+        System.out.println("*You are now in the first grade at Poudlard. Here is your first class.*\n"+
+                "Teacher : Welcome every one, let's learn our first easy spell : Wingardium Leviosa.");
     }
 }

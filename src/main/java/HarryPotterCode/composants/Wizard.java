2ba21f6a4efe8465c55499299884b52a0f1dd793
nicolabrico <niga61368@eleve.isep.fr>
2023-02-25 10:05:38 +0100

    Création de la classe Wizzard

--- src/main/java/HarryPotterCode/composants/Wizard.java
+++ src/main/java/HarryPotterCode/composants/Wizard.java
@@ -3,9 +3,20 @@ package HarryPotterCode.composants;
 import java.util.List;
 
 public class Wizard {
-    private class pet(Pet){}
-    private class wand(Wand){}
-    private class house(House){}
-    private class knownSpells(List<Spell>){}
-    private class potion(List<Potion>){}
+    private Pet pet;
+    private Wand wand;
+    private House house;
+    private List<Spell> knownSpells;
+    private List<Potion> potions;
+    public Wizard(Pet pet, Wand wand, House house, List<Spell> knownSpells, List<Potion> potions) {
+        this.pet = pet;
+        this.wand = wand;
+        this.house = house;
+        this.knownSpells = knownSpells;
+        this.potions = potions;
+    }
+
+    public void defend() {
+        // code de la méthode defend
+    }
 }

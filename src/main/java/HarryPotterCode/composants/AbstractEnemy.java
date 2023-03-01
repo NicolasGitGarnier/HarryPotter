1a841427166d286a470d02c018f0f9c2f731ef4d
nicolabrico <niga61368@eleve.isep.fr>
2023-03-01 10:03:37 +0100

    Correction bug constructeur

--- src/main/java/HarryPotterCode/composants/AbstractEnemy.java
+++ src/main/java/HarryPotterCode/composants/AbstractEnemy.java
@@ -1,8 +1,10 @@
 package HarryPotterCode.composants;
 
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class AbstractEnemy extends Character{
     public AbstractEnemy(int health, int defence, int damage, int accuracy, int dodge) {
         super(health, defence, damage, accuracy, dodge);

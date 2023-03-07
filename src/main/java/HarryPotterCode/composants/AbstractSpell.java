e91adaedea6b047ebf06a62c5050ba664cee3c42
nicolabrico <niga61368@eleve.isep.fr>
2023-03-07 18:12:08 +0100

    Annulation de : Ajout pourcentage réussite lancé de sort ou non v1. Inutile

--- src/main/java/HarryPotterCode/composants/AbstractSpell.java
+++ src/main/java/HarryPotterCode/composants/AbstractSpell.java
@@ -11,10 +11,5 @@ import java.util.Random;
 @NoArgsConstructor
 public class AbstractSpell {
     private String name;
-    private int sc; /* sc stand for succes chance */
-
-    public int sp() { /* sp stand for : succes pourcentage */
-        sc = new Random().nextInt(21)+80; /* random number between 20 and 100 */
-        return sc;
-    }
+    private int succesChance;
 }

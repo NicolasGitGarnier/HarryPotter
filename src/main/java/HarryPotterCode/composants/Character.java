fb07505ce0f96399326113628d3b07aef30b3aff
nicolabrico <niga61368@eleve.isep.fr>
2023-03-07 17:53:29 +0100

    Correction de la fonction attack

--- src/main/java/HarryPotterCode/composants/Character.java
+++ src/main/java/HarryPotterCode/composants/Character.java
@@ -17,22 +17,23 @@ public class Character {
     private String name;
 
     public Character(int health, int defence, int damage, int accuracy) {
-        this.health = 10;
-        this.defence = 0;
-        this.damage = 2;
-        this.accuracy = 5;
+        this.health = health;
+        this.defence = defence;
+        this.damage = damage;
+        this.accuracy = accuracy;
     }
 
     public int attack(Character target) {
         int damage;
         if (target instanceof Enemy) {
             // Wizard attack enemy
-            damage = (this.damage + this.potionDamage) - (target.getDefence() * (this.damage + this.potionDamage) / 100);
+            damage = (this.damage + this.potionDamage) - (( target.getDefence() / 100 ) * this.damage + this.potionDamage);
         } else {
             // Enemy attack wizard
-            damage = target.getDamage() - (this.defence * target.getDamage() / 100);
+            damage = this.damage - (target.getDefence() * ( target.getDamage() / 100 ));
         }
         target.setHealth(target.getHealth() - damage);
         return damage;
     }
+
 }

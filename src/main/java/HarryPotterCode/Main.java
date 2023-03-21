cc4624e79568fc086ec65bca73db36e82fd5e11e
nicolabrico <niga61368@eleve.isep.fr>
2023-03-21 14:25:31 +0100

    Level5 vF

--- src/main/java/HarryPotterCode/Main.java
+++ src/main/java/HarryPotterCode/Main.java
@@ -6,7 +6,6 @@ import HarryPotterCode.composants.Others.SortingHat;
 import HarryPotterCode.composants.Spells.Spell;
 import HarryPotterCode.composants.WandStuff.Wand;
 
-import java.util.Random;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -32,26 +31,31 @@ public class Main {
         Introduction introduction = new Introduction();
         introduction.runi(wizard);
 
-        Spell.learnWingardium(wizard.getKnownSpells());
-        Spell.learnAccio(wizard.getKnownSpells());
-        Spell.learnExpectoPatronum(wizard.getKnownSpells());
+        /* Only use : for me when i skip levels and test my latest level release.*/
+         Spell.learnWingardium(wizard.getKnownSpells());
+         Spell.learnAccio(wizard.getKnownSpells());
+         Spell.learnExpectoPatronum(wizard.getKnownSpells());
+
+        /*
         // --------------- Level 1 --------------- //
-        //Level1 level1 = new Level1(wizard);
-        //level1.run1(wizard);
+        Level1 level1 = new Level1(wizard);
+        level1.run1(wizard);
 
         // --------------- Level 2 --------------- //
-        //Level2 level2 = new Level2(wizard);
-        //level2.run2(wizard);
+        Level2 level2 = new Level2(wizard);
+        level2.run2(wizard);
 
         // --------------- Level 3 --------------- //
-        //Level3 level3 = new Level3(wizard);
-        //level3.run3(wizard);
+        Level3 level3 = new Level3(wizard);
+        level3.run3(wizard);
 
         // --------------- Level 4 --------------- //
         Level4 level4 = new Level4(wizard);
-        level4.run4(wizard);
+        level4.run4(wizard);*/
 
         // --------------- Level 5 --------------- //
+        Level5 level5 = new Level5(wizard);
+        level5.run5(wizard);
 
         // --------------- Level 6 --------------- //
 
---
+++ src/main/java/HarryPotterCode/composants/Levels/Level5.java
@@ -0,0 +1,68 @@
+package HarryPotterCode.composants.Levels;
+
+import HarryPotterCode.composants.Characters.Enemy;
+import HarryPotterCode.composants.Characters.Wizard;
+import HarryPotterCode.composants.Spells.Spell;
+import lombok.AllArgsConstructor;
+
+import java.util.Random;
+
+import static HarryPotterCode.composants.Characters.Wizard.levelUpLevels;
+
+@AllArgsConstructor
+public class Level5 {
+    private Wizard wizard;
+    public void run5(Wizard wizard) throws InterruptedException {
+        System.out.println("* Welcome in your fifth years in Hogwards, this year is quite special because there is the BUSE exam, plus a big garden party at the end. *");
+        Thread.sleep(2500);
+        System.out.println("* To prepare the garden party you need firework to surprise every one. However you need Fredd and George (in your class) to create those ones before. *");
+        Thread.sleep(3000);
+        System.out.println("* Unfortunately you are with Ombrage for your class, so you need to distract her to secure Fredd and George ! *");
+        Thread.sleep(2000);
+        GetFirework();
+        Thread.sleep(2000);
+        System.out.println("* Everything went good. You have been diplomed, well done ! *");
+        System.out.println("* Your fifth year is now finished ! *");
+        levelUpLevels(wizard);
+    }
+    private void GetFirework() throws InterruptedException{
+        boolean haveFirework = false;
+        int probaGetFirework = 10;
+        while (!haveFirework){
+            System.out.println("* Disturb Ombrage ! Move objects with spells to annoy her until you get fireworks... *");
+            Thread.sleep(2000);
+            CastSpellLevel5();
+            probaGetFirework = probaGetFirework + 20;
+            int random = new Random().nextInt(100); // Calculate the chance to have fireworks.
+            if (random < probaGetFirework){
+                haveFirework = true;
+                Thread.sleep(2000);
+                System.out.println("* Fredd and Georg finally make pop Fireworks in your Wizard's coat ! They are ready to use. You can dismiss your class with Ombrage ! *");
+                System.out.println("* You left the class ... *");
+            }
+        }
+    }
+
+    private void CastSpellLevel5() throws InterruptedException{
+
+        Spell spell = wizard.chooseSpell(wizard.getKnownSpells());// Wizard need to choose a spell in his list of spells.
+        boolean castSucces = Spell.castSpell(spell, wizard);// We defined if the wizard succed to cast his spell.
+        if (castSucces) {// If he succes to cast the spell he attack.
+
+            switch (spell.getName()) {
+                case "Wingardium Leviosa" -> {
+                    Thread.sleep(1000);
+                    System.out.println("* You move an object around her back and distract her for a minute... *");
+                }
+                case "Accio" -> {
+                    Thread.sleep(1000);
+                    System.out.println("* You move an object behind her back and distract her for a minute... *");
+                }
+                case "Expecto Patronum" -> {
+                    Thread.sleep(1000);
+                    System.out.println("* No you do not want to cast this spell to distract her... *");
+                }
+            }
+        }
+    }
+}

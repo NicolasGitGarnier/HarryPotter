6c23f87cc1b86cd2ad25b0504c4a189b2f489b76
nicolabrico <niga61368@eleve.isep.fr>
2023-03-26 18:05:02 +0200

    TO DO list update

--- src/main/java/HarryPotterCode/composants/TODO.java
+++ src/main/java/HarryPotterCode/composants/TODO.java
@@ -3,13 +3,8 @@ package HarryPotterCode.composants;
 /*
 ================================================ To Do =================================================================
 
-- Rework accuracy // Done
-- Rework diff btwn Accio and Wingardium // Done
-
-- Potion system and optention
 - Correction code avec les this. et les super => lomebok supérieur
-- Suppression des statics
-- Chap 6 / 7
+- Chap 7
 
 ========================================================================================================================
  */
